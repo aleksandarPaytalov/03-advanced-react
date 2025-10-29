437309dda61679a2d5e026363f9c76ca0b7f4311
aleksandarPaytalov <apaytalov89@gmail.com>
2025-10-29 18:28:16 +0200

    04-useState-object-exercise

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,26 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [name, setName] = useState("Alex");
+  const [age, setAge] = useState(29);
+  const [hobby, setHobby] = useState("Learn React Vite");
+
+  const createPerson = () => {
+    setName("Gosho");
+    setAge(35);
+    setHobby("Snowboarding");
+  };
+
+  return (
+    <div>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h4>Enjoy to: {hobby}</h4>
+      <button type="button" className="btn" onClick={createPerson}>
+        Change person
+      </button>
+    </div>
+  );
 };
 
 export default UseStateObject;
