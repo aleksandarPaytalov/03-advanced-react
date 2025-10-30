54ddd0cd572014f4950ef71b5463474db3a62ba2
aleksandarPaytalov <apaytalov89@gmail.com>
2025-10-30 12:48:27 +0200

    useState object example - exercise

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,21 +1,29 @@
 import { useState } from "react";
 
 const UseStateObject = () => {
-  const [name, setName] = useState("Alex");
-  const [age, setAge] = useState(29);
-  const [hobby, setHobby] = useState("Learn React Vite");
+  const [person, newPerson] = useState({
+    name: "Alex",
+    age: 35,
+    hobby: "learning Vite",
+  });
+
+  // const [name, setName] = useState("Alex");
+  // const [age, setAge] = useState(29);
+  // const [hobby, setHobby] = useState("Learn React Vite");
 
   const createPerson = () => {
-    setName("Gosho");
-    setAge(35);
-    setHobby("Snowboarding");
+    // setName("Gosho");
+    // setAge(35);
+    // setHobby("Snowboarding");
+    // newPerson({ name: "ivan", age: 29, hobby: "snowboarding" });
+    newPerson({ ...person, name: "ivan", age: 22 });
   };
 
   return (
     <div>
-      <h3>{name}</h3>
-      <h3>{age}</h3>
-      <h4>Enjoy to: {hobby}</h4>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h4>Enjoy to: {person.hobby}</h4>
       <button type="button" className="btn" onClick={createPerson}>
         Change person
       </button>
@@ -24,3 +32,4 @@ const UseStateObject = () => {
 };
 
 export default UseStateObject;
+//
