e9c0d0d47463fc35ce9f6de602e7f0ca96773f1c
aleksandarPaytalov <apaytalov89@gmail.com>
2025-12-06 10:59:50 +0200

    custom hook

--- src/tutorial/09-context-api/starter/Navbar.jsx
+++ src/tutorial/09-context-api/starter/Navbar.jsx
@@ -1,8 +1,8 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useContext } from "react";
 import NavLinks from "./NavLinks";
-import { use } from "react";
 
 export const NavbarContext = createContext();
+export const useAppContext = () => useContext(NavbarContext);
 
 const Navbar = () => {
   const [user, setUser] = useState({ name: "bob" });
@@ -11,12 +11,10 @@ const Navbar = () => {
   };
   return (
     <NavbarContext.Provider value={{ user, logout }}>
-      <na>
-        <nav className="navbar">
-          <h5>CONTEXT API</h5>
-          <NavLinks />
-        </nav>
-      </na>
+      <nav className="navbar">
+        <h5>CONTEXT API</h5>
+        <NavLinks />
+      </nav>
     </NavbarContext.Provider>
   );
 };
