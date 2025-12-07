4fb67f24ebd878958f66ff449e4e106cc311edd8
aleksandarPaytalov <apaytalov89@gmail.com>
2025-12-07 14:46:13 +0200

    lower state - additional exercise challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from "./tutorial/11-performance/starter/01-lower-state/index";
+import Starter from "./tutorial/11-performance/starter/02-lower-state-challenge/index";
 
 function App() {
   return (
